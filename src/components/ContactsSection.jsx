802a6a8665987251a0ccf688a4de8f090996b6aa
ruter <arturkummer08@gmail.com>
2026-02-27 16:45:41 +0100

    linkedin

--- src/components/ContactsSection.jsx
+++ src/components/ContactsSection.jsx
@@ -5,7 +5,7 @@ function ContactsSection() {
     <section key="contacts">
       <h1>Contacts</h1>
       <p><FaEnvelope style={{verticalAlign:'middle',marginRight:8,color:'#667eea'}}/>Email: <a href="mailto:contact@rutra.me" style={{color:'#667eea',textDecoration:'underline'}}>contact@rutra.me</a></p>
-      <p><FaLinkedin style={{verticalAlign:'middle',marginRight:8,color:'#764ba2'}}/>LinkedIn: <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer" style={{color:'#764ba2',textDecoration:'underline'}}>linkedin.com/</a></p>
+      <p><FaLinkedin style={{verticalAlign:'middle',marginRight:8,color:'#764ba2'}}/>LinkedIn: <a href="https://www.linkedin.com/in/artur-k-7b6718340/" target="_blank" rel="noopener noreferrer" style={{color:'#764ba2',textDecoration:'underline'}}>linkedin.com/rutra</a></p>
       <p><FaGithub style={{verticalAlign:'middle',marginRight:8,color:'#fff'}}/>GitHub: <a href="https://github.com/rutra8002" target="_blank" rel="noopener noreferrer" style={{color:'#fff',textDecoration:'underline'}}>github.com/rutra8002</a></p>
     </section>
   );
